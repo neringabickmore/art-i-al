32a0ad5ba840455e0e380e37260683f9089b3ef2
neringa <neringa.bickmore@gmail.com>
2021-01-27 23:10:55 +0000

    PRODUCT/Add product detail page content and css

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('gallery/', views.gallery, name='gallery'),
     path('shop/', views.shop, name='shop'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
