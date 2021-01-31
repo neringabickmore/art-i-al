c8f1abbd0fe23c7d86bf483125af62280bfb6bff
neringa <neringa.bickmore@gmail.com>
2021-01-31 21:09:30 +0000

    HOME/Created and updated models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import About, SocialMedia
 
-# Register your models here.
+class AboutAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'img',
+        'url',
+    )
+
+class SocialMediaAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'icon',
+        'url',
+        'on'
+    )
+    ordering = ('name',)
+
+admin.site.register(About, AboutAdmin)
+admin.site.register(SocialMedia, SocialMediaAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class About(models.Model):
+
+    class Meta:
+        verbose_name_plural = "About Section"
+
+    name = models.CharField(max_length=254)
+    description = models.TextField(max_length=800)
+    img = models.ImageField(null=True, blank=True) 
+    url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SocialMedia(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Social Media Icons"
+
+    name = models.CharField(max_length=254)
+    icon = models.CharField(max_length=50)
+    url = models.URLField(max_length=1024, default= '', null=True, blank=True)
+    on = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
