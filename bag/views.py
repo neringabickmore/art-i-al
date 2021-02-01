ba2e5b955f4f351b9addf500ef0ea1886f959f89
neringa <neringa.bickmore@gmail.com>
2021-02-01 21:35:49 +0000

    BAG/Initiated the app and rendered urls

---
+++ bag/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import (
+    render, redirect, reverse,
+    HttpResponse, get_object_or_404
+)
+
+from products.models import Product
+
+# Create your views here.
+
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
