29fe737738a9fe62822b3035b86671d03361c753
neringa <neringa.bickmore@gmail.com>
2021-02-03 21:06:11 +0000

    BAG/ADD_TO_BAG/Added functionality

--- bag/views.py
+++ bag/views.py
@@ -11,3 +11,22 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, product_id):
+    """ 
+    Add the specified product 
+    to the shopping bag 
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    bag[product_id] = quantity
+    
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    
+    return redirect(redirect_url)
