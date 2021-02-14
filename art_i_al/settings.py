d0360dc786dccc7cebab5cf66c20bcd9dcf4670c
neringa <neringa.bickmore@gmail.com>
2021-02-14 17:39:32 +0000

    deployment/initiated deployment set up/part 1

--- art_i_al/settings.py
+++ art_i_al/settings.py
@@ -16,6 +16,8 @@ from os import path
 if path.exists("env.py"): 
     import env
 
+import dj_database_url
+
 dev = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,12 +115,17 @@ WSGI_APPLICATION = 'art_i_al.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
