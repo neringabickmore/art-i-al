70c9ec93e4d81eb5a5895b275cdede6f3f700a7d
neringa <neringa.bickmore@gmail.com>
2021-02-15 22:41:50 +0000

    settings/added media context processors

--- art_i_al/settings.py
+++ art_i_al/settings.py
@@ -88,6 +88,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', #Required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
         },
