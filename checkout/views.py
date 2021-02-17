b7d01b55ae50df45100ec50cb33514bf125c2d31
neringa <neringa.bickmore@gmail.com>
2021-02-17 22:27:07 +0000

    views/checkout-app/added basic views information

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import sweetify
 
-# Create your views here.
+from django.shortcuts import render, redirect, reverse
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        sweetify.error(request, title='error', icon='error',
+        text= "There's nothing in your bag at the moment",
+        timer=2000, timerProgressBar='true', persistent="Close")
+        return redirect(reverse('shop'))
+
+    order_form = OrderForm()
+    
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
