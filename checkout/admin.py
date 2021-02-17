c453f8ec33aadfee58a690040da5e92b12d0ea37
neringa <neringa.bickmore@gmail.com>
2021-02-17 21:41:35 +0000

    admin/Checkout-app/Added classes and registered

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('line_item_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'purchase_date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'purchase_date', 'full_name',
+              'email', 'phone_number', 'street_address1',
+              'street_address2', 'town_or_city', 'county','country',
+              'postcode', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'purchase_date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-purchase_date',)
+
+admin.site.register(Order, OrderAdmin)
