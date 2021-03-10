e927a6999a075ff5694a1b1d6b17b5483a4b288d
neringa <neringa.bickmore@gmail.com>
2021-03-10 23:36:49 +0000

    Real email/set up

--- art_i_al/settings.py
+++ art_i_al/settings.py
@@ -215,4 +215,16 @@ STRIPE_CURRENCY = os.environ.get('STRIPE_CURRENCY')
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
-DEFAULT_FROM_EMAIL = 'art.ial@example.com'
+
+# Email set up
+if 'DEBUG' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'art.ial@example.com'
+else: 
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL= os.environ.get('EMAIL_HOST_USER')
