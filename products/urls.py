c21095c570f54c6c4a5cfa8d21660e4fdea80fbf
neringa <neringa.bickmore@gmail.com>
2021-03-13 21:14:05 +0000

    Product urls/updated existing and added collection

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('gallery/', views.gallery, name='gallery'),
     path('shop/', views.shop, name='shop'),
-    path('<int:product_id>/', views.product_detail, name='product_detail'), 
-    path('products/add-product/', views.add_product, name='add_product'),
+    path('<name>/', views.product_detail, name='product_detail'), 
+    path('add/product/', views.add_product, name='add_product'),
+    path('add/collection/', views.add_collection, name='add_collection'),
 ]
