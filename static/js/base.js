bda8e1fa0f1d8d68c1dbeab312be70d089957705
neringa <neringa.bickmore@gmail.com>
2021-03-15 23:12:50 +0000

    Loader/ Added loader and styled it

--- static/js/base.js
+++ static/js/base.js
@@ -1,4 +1,12 @@
+/**
+ * While document loads, show loader
+ * original code idea: 
+ * https://stackoverflow.com/questions/3623890/jquery-onload-function
+ */
+$(document).ready(function () {
+  $('.loader-overlay-wrapper').fadeOut(1200);
+})
 // loads tooltips 
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  $('[data-toggle="tooltip"]').tooltip()
+})
