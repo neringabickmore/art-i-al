53a5a4639c46dcd3fa1df637aa7ff6f27f64dd15
neringa <neringa.bickmore@gmail.com>
2021-03-16 22:18:59 +0000

    Go-up-btn/Added button, fnctionality and style

--- static/js/base.js
+++ static/js/base.js
@@ -10,3 +10,43 @@ $(document).ready(function () {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+const goUpBtn = document.getElementById("goUpBtn");
+
+/**
+ * When the user scrolls down 
+ * 20px from the top of the document
+ * show the button.
+ */
+ window.onscroll = function() {
+   scrollFunction();
+  };
+
+ /**
+  * Scroll function:
+  * when the user scrolls the page
+  * the button to go up appears. 
+  * Otherwise, the button remains invisible. 
+  */
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goUpBtn.style.display = "block";
+  } else {
+    goUpBtn.style.display = "none";
+  }
+};
+
+/**
+ * When the user clicks on the button
+ * scroll to the top of the document
+ */
+const topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+// Event Listener
+
+$("#goUpBtn").click(function () {
+  topFunction();
+});
