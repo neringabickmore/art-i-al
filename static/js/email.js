008672f993829bbde592ea7df4855ada58816ec4
neringa <neringa.bickmore@gmail.com>
2021-04-02 11:59:52 +0100

    email.js/updated message content

--- static/js/email.js
+++ static/js/email.js
@@ -1,6 +1,6 @@
 // defence for deleting email by accident
 (function () {
-    let message = "Do you really want to remove the selected e-mail address?";
+    let message = "Do you really want to remove the selected e-mail address? If this is your only email it will not be removed.";
     let actions = document.getElementsByName('action_remove');
     if (actions.length) {
         actions[0].addEventListener("click", function (e) {
